62c62887ce6f0e09c3655f4eaf9fb15527b4015a
sandeep yadav <sandeep.yadav@axiomtelecom.com>
2023-02-20 14:53:51 +0400

    Updated element name in homepage class

--- src/main/java/pages/HomePageElements.java
+++ src/main/java/pages/HomePageElements.java
@@ -17,14 +17,14 @@ public class HomePageElements {
 	}
 
 	// All objects should be defined here
-	private By forgotPassword_Lbl = By.xpath("//button[contains(text(),'Top Up')]");
+	private By topUp_Lbl = By.xpath("//button[contains(text(),'Top Up')]");
 	private By logout_Btn = By.xpath("//button[contains(text(),'Logout')]");
 	private By header_Lbl = By.xpath("//header/ul[3]/li[1]/a[1]");
 
 	// To check Home page is visible after login
 	public void checkHomePageAfterLogin(String url) {
 		Assert.assertTrue(driver.getCurrentUrl().equals(url));
-		Assert.assertTrue(commonActions.checkElementIsDisplayed(forgotPassword_Lbl));
+		Assert.assertTrue(commonActions.checkElementIsDisplayed(topUp_Lbl));
 	}
 
 	// Open Profile and click on logout button on Home page
